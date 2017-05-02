1315d6b7ac97d9f6083eaee19c442821a5c05488
Adam Freedman <adamfreedman@fas.harvard.edu>
2017-05-02 11:05:51 -0400

    return dict with read numbers as keys for parse_runinfoxml

--- parsers/parse_runinfoxml.py
+++ parsers/parse_runinfoxml.py
@@ -9,6 +9,9 @@ def get_readinfo_from_runinfo(runinfo_xml_file):
     for child in root.iter('Reads'):
         for child in child:
             read_dicts.append(child.attrib)
-
-    return read_dicts
+    readkey_to_readdata_map={}
+    for read_dict in read_dicts:
+        number=read_dict.pop('Number')
+        readkey_to_readdata_map['read%s' % number] = read_dict
+    return readkey_to_readdata_map
 
