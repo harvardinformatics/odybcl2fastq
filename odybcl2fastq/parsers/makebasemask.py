f0ce34f391fdf7c2c6c8da32919ee5dc1a20a83b
Adam Freedman <adamfreedman@fas.harvard.edu>
2017-05-03 15:59:31 -0400

    added basemask string from xml creator

--- odybcl2fastq/parsers/makebasemask.py
+++ odybcl2fastq/parsers/makebasemask.py
@@ -7,7 +7,7 @@ from odybcl2fastq.parsers.parse_runinfoxml import get_readinfo_from_runinfo
 from odybcl2fastq.parsers.parse_sample_sheet import sheet_parse
 from odybcl2fastq import UserException
 
-def check_runinfo_samplesheet_consistency(readdict,sampledict):
+def check_indexlength_consistency(readdict,sampledict):
     index_lengths={'index1':'','index2':''}
     for read in readdict.keys():
         if read == 'read2' and readdict[read]['IsIndexedRead'] == 'Y':
@@ -15,7 +15,6 @@ def check_runinfo_samplesheet_consistency(readdict,sampledict):
         elif read == 'read3' and readdict[read]['IsIndexedRead'] == 'Y':
             index_lengths['index2'] = int(readdict[read]['NumCycles'])     
     
-    print 
     for sample in sampledict.keys():
         if sampledict[sample]['index'] == '' and index_lengths['index1']!= '':
             raise UserException('Index sequence not present in %s' % sample)
@@ -26,17 +25,32 @@ def check_runinfo_samplesheet_consistency(readdict,sampledict):
         elif len(sampledict[sample]['index2']) != index_lengths['index2']:
             return False
         else: 
-            return True
+            return True 
 
-def extract_basemask(runinfo,sample_sheet):
-    rundata_by_read=get_readinfo_from_runinfo(runinfo)
-    data_by_sample=sheet_parse(sample_sheet)['Data']
+
+def extract_basemasks(runinfo,sample_sheet):
+    rundata_by_read = get_readinfo_from_runinfo(runinfo)
+    data_by_sample = sheet_parse(sample_sheet)['Data']
     
-    masktest=check_runinfo_samplesheet_consistency(rundata_by_read,data_by_sample)
-    if masktest == True:
-        return True
-    elif masktest == False:
-        return False
+    mask_by_read=[]
+    for read in rundata_by_read.keys():
+        if rundata_by_read[read]['IsIndexedRead'] == 'Y' and rundata_by_read[read]['NumCycles'] == '7':
+            mask_value = int(rundata_by_read[read]['NumCycles'])-1
+            mask_by_read.append('i%s' % mask_value)
+        elif rundata_by_read[read]['IsIndexedRead'] == 'Y' and rundata_by_read[read]['NumCycles'] != '7':
+            mask_by_read.append('i%s' % rundata_by_read[read]['NumCycles'])
+        elif rundata_by_read[read]['IsIndexedRead'] == 'N':
+            mask_value=int(rundata_by_read[read]['NumCycles'])-1
+            mask_by_read.append('i%s' % mask_value)      
+        
+    maskstring=','.join(mask_by_read)
+    return rundata_by_read,data_by_sample,maskstring     
+        
+
+
+    ### need to use recipe next to see if different indices by lane or diff within lane     
+        
+          
         
         
 
