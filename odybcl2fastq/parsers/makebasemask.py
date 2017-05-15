f3c2544f9a4eed41352339dcd72e725c9c01cc22
Adam Freedman <adamfreedman@fas.harvard.edu>
2017-05-15 16:43:25 -0400

    added hiseq mask collapse recursion

--- odybcl2fastq/parsers/makebasemask.py
+++ odybcl2fastq/parsers/makebasemask.py
@@ -5,7 +5,7 @@ can be written for each run/lane
 from odybcl2fastq.parsers.parse_runinfoxml import get_readinfo_from_runinfo
 from odybcl2fastq.parsers.parse_sample_sheet import sheet_parse
 from odybcl2fastq import UserException
-from collections import OrderedDict
+from collections import OrderedDict,defaultdict
 from copy import copy
 from sets import Set
 
@@ -41,21 +41,26 @@ def make_hiseq_mask(universal_mask,sample_key,sample_dict):
 
     return lane,mask            
 
-"""
-def collapse_hiseq_masks(sample_masks):
-    #number_lanes=int(sample_masks.keys()[-1].split(':'))
-    #masks_by_lane=OrderedDict()
-    #for mask in sample_masks.values():
-        #if mask.split(':')[1] not in masks_by_lane:
-            #masks_by_lane[mask.split(':')[1]]=Set()
-        #masks_by_lane[mask.split(':')[1]].add(mask.split(':')[0])
 
-    #if len(masks_by_lane) == 1: # only 1 mask for all lanes
-        #return masks_by_lane.keys()
+def collapse_hiseq_masks(mask_lanes_dict,queues): # unique_masks is a python set
+    queue = {}
+    masks = mask_lanes_dict.keys()
+    for mask in masks:
+        lanes = mask_lanes_dict[mask]
+        for i,lane in enumerate(lanes):
+            if lane not in queue.keys():
+                queue[lane] = mask
+                mask_lanes_dict[mask].pop(i)
+        if len(mask_lanes_dict[mask]) == 0:
+            mask_lanes_dict.pop(mask)                 
+                    
+    queues.append(queue) 
+    if len(mask_lanes_dict) != 0:
+        collapse_hiseq_masks(mask_lanes_dict,queues)
 
-    #else: 
-"""        
-
+    return queues
+    
+     
 def extract_basemasks(runinfo,sample_sheet):
     rundata_by_read = get_readinfo_from_runinfo(runinfo)
     data_by_sample = sheet_parse(sample_sheet)['Data']
@@ -67,12 +72,23 @@ def extract_basemasks(runinfo,sample_sheet):
         for sample in data_by_sample.keys():
             lane,mask = make_hiseq_mask(universal_mask,sample,data_by_sample[sample])
             sample_masks[sample] = ':'.join([lane,mask])
-            unique_masks.add(sample_masks[sample])            
-                                   
+            unique_masks.add(sample_masks[sample]) 
+           
+        if len(Set([mask.split(':')[1] for mask in unique_masks])) == 1:
+            return list(Set([mask.split(':')[1] for mask in unique_masks]))
+        else:
+            mask_lanes_dict = defaultdict(list)
+            for mask in unique_masks:
+                mask_lanes_dict[mask.split(':')[1]].append(mask.split(':')[0])
+            queues = []
+            newmasks=defaultdict(list)
+            unique_masks = collapse_hiseq_masks(mask_lanes_dict,queues)
+            return unique_masks 
+                
+ 
     else:
         for sample in data_by_sample.keys():
             sample_mask = make_nextseq_mask(universal_mask)    
             sample_masks[sample] = sample_mask
             unique_masks.add(sample_masks[sample])
-                    
-    return rundata_by_read,data_by_sample,universal_mask,sample_masks,unique_masks        
+        return unique_masks        
