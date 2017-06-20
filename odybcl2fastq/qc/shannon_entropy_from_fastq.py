f51c98ea66c9a00bd8dda03d05b0431b7250960b
afreedman <adamfreedman@fas.harvard.edu>
2017-06-20 11:26:17 -0400

    fixed infinite loop

--- odybcl2fastq/qc/shannon_entropy_from_fastq.py
+++ odybcl2fastq/qc/shannon_entropy_from_fastq.py
@@ -46,20 +46,20 @@ def shannon_entropy(base_freq_dict):
 def entropy_runner(fqin,samplingfreq,max_sample_count = 100000):
     readstream = get_input_stream(fqin)
     entropy_values = []
-    
+    counter=0    
     with readstream as stream:
         reads=grouper(stream,4)
-        while len(entropy_values) <= max_sample_count:
-            for read in reads:        
-                prob_sampling=random.uniform(0,1)
-                if prob_sampling<=samplingfreq and len(enotropy_values)<=max_sample_count:
-                    head,seq,placeholder,qual=[i.strip() for i in read]
-                    nstrip_read=stripNs(seq)
-                    nucleotide_freqs=calc_base_freqs(nstrip_read)
-                    read_entropy=shannon_entropy(nucleotide_freqs)
-                    entropy_values.append(read_entropy)
-        	    	
-
+        for read in reads:
+            prob_sampling=random.uniform(0,1)
+            if prob_sampling<=samplingfreq: 
+                head,seq,placeholder,qual=[i.strip() for i in read]
+                nstrip_read = stripNs(seq)
+                nucleotide_freqs = calc_base_freqs(nstrip_read)
+                read_entropy = shannon_entropy(nucleotide_freqs)
+                entropy_values.append(read_entropy)
+            if len(entropy_values) == max_sample_count:
+                break     	    	
+         
         entropy_summary=OrderedDict()
         entropy_summary['nsamples'] = len(entropy_values)
         entropy_summary['mean'] = mean(entropy_values)
@@ -71,12 +71,12 @@ def entropy_runner(fqin,samplingfreq,max_sample_count = 100000):
 
     return entropy_summary
 
-def write_entropy_report(fqin,samplingfreq,**kwargs):
-    entropy_summary=entropy_runner(fqin,samplingfreq,**kwargs)
-    filestring = basename(fqin.replace('.fastq','').replace('.gz','')
+def write_entropy_report(fqin,samplingfreq,maxcount):
+    entropy_summary=entropy_runner(fqin,samplingfreq,max_sample_count=maxcount)
+    filestring = basename(fqin.replace('.fastq','').replace('.gz',''))
     report = open('%s_entropy.txt' % filestring,'w')
     for key in entropy_summary.keys()[:-1]:
-        report.write('%s:\t%s\n' % (key,entropy_summary_keys[key])
+        report.write('%s:\t%s\n' % (key,entropy_summary_keys[key]))
     report.close()
     
     vals_R_vector='vals=c(%s)' % ','.join([str(val) for val in entropy_summary['vector']])
@@ -91,9 +91,9 @@ def write_entropy_report(fqin,samplingfreq,**kwargs):
     
     makeplot = 'Rscript %s_entropyhist.R' % filestring
     plot_run = Popen(makeplot,shell=True,stderr=PIPE,stdout=PIPE)
-        plotrun_out,plotrun_err=plot_run.communicate() 
-        if plot_run.returncode!=0:
-            print('Error generating entropy histogram for %s: %s\n' % (fqin,plotrun_err))
+    plotrun_out,plotrun_err=plot_run.communicate() 
+    if plot_run.returncode!=0:
+        print('Error generating entropy histogram for %s: %s\n' % (fqin,plotrun_err))
 
     Rcode.close()
     
@@ -102,7 +102,7 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description="options for calculating entropy from a fastq_file")
     parser.add_argument('-i','--fastqin',dest='fqin',type=str,help='fastq infile')
     parser.add_argument('-f','--subsample_freq',default=0.001,dest='subsamp',type=float,help='proportion of sequences to print entropy for')
-    parser.add_argument('-m','--max_samples',default=100000,dest=maxsamp,type=int,help='max number of reads to calculate entropy')
+    parser.add_argument('-m','--max_samples',default=100000,dest='maxsamp',type=int,help='max number of reads to calculate entropy')
     opts = parser.parse_args()
     
     write_entropy_report(opts.fqin,opts.subsamp,opts.maxsamp)
