ffa880fa9dcabd6d136f4ea5fddaf06613b9677c
afreedman <adamfreedman@fas.harvard.edu>
2017-06-23 11:58:44 -0400

    dustmasker code fixed

--- odybcl2fastq/qc/dustmasker_runner.py
+++ odybcl2fastq/qc/dustmasker_runner.py
@@ -14,7 +14,7 @@ from numpy import mean,median,log2,random,std,histogram
 from scipy.stats import sem
 from collections import defaultdict,OrderedDict
 from os.path import basename
-from subprocess import Popen,PIPE
+from subprocess import Popen,PIPE,call
 from odybcl2fastq import UserException
 
 
@@ -37,42 +37,51 @@ def fastq_sampler(fqin,samplingfreq=0.01,max_sample_count=100000):
     fastaout=open(fastaname,'w')
     readstream = get_input_stream(fqin)
     counter=0
+    read_lengths={}
     with readstream as stream:
         reads=grouper(stream,4)
         for read in reads:        
             prob_sampling=random.uniform(0,1)
-            if prob_sampling<=samplingfreq and counter < max_sample_count:
+            if prob_sampling<=samplingfreq:
                 head,seq,placeholder,qual=[i.strip() for i in read]
+                head = head.split()[0]                
         	counter+=1
                 fastaout.write('>%s\n%s\n' % (head.split()[0],seq))
+                read_lengths[head[1:]] = len(seq)
+            if counter == max_sample_count:
+                break
     
     fastaout.close()
-    return fastaname
+    return fastaname,read_lengths
     
 
 def dustmasker_runner(fasta,windowsize=20):             	
-    dustcmd = 'dustmasker -in %s -out dustmasked_%s -window %s' % (fasta,fasta,windowsize) 
+    dustcmd = 'dustmasker -in %s -out dustmasked_%s.intervals -window %s -outfmt acclist' % (fasta,fasta,windowsize) 
     dust_run = Popen(dustcmd,shell=True,stderr=PIPE,stdout=PIPE)
-        dust_out,dust_err=dust_run.communicate() 
-        if dust_run.returncode!=0:
-            raise Exception('dustmasker run on %s failed: %s' % (fasta,dust_err)               
-
-    return 'dustmasked_%s' % fasta       
+    dust_out,dust_err=dust_run.communicate() 
+    if dust_run.returncode!=0:
+        raise Exception('dustmasker run on %s failed: %s' % (fasta,dust_err))               
+    else:
+        return 'dustmasked_%s.intervals' % fasta       
 
 def dust_summarize(fastqin,windowsize=20,samplingfreq=0.01,max_sample_count=100000):
-
-    sampled_as_fasta = fastq_sampler(fastqin,samplingfreq=samplingfreq,max_sample_count=max_sample_count)
-    dustmasked_fasta = dustmasker_runner(sampled_as_fasta,windowsize=windowsize)
-
     masked_proportions = []
-    readstream = get_input_stream(fqin)
-    counter=0
-    with readstream as stream:
-        reads=grouper(stream,4)
-        for read in reads:
-            head,seq,placeholder,qual=[i.strip() for i in read]
-            
-    masked_proportions.append(seq.count('N')/float(len(seq)))
+    sampled_as_fasta,read_lengths = fastq_sampler(fastqin,samplingfreq=samplingfreq,max_sample_count=max_sample_count)
+    fasta_open=open(sampled_as_fasta,'r')
+    print 'read lengths', read_lengths   
+    dustmasked_intervals = dustmasker_runner(sampled_as_fasta,windowsize=windowsize)
+    read_mask_dict={}
+    interval_parse=open(dustmasked_intervals,'r')
+    for line in interval_parse:
+        linelist=line.replace('>@','').strip().split('\t')
+    
+        read_mask_dict[linelist[0]] = (int(linelist[2]) - int(linelist[1]) + 1)/float(read_lengths[linelist[0]])
+    for read in read_lengths.keys():
+        if read not in read_mask_dict:
+            masked_proportions.append(0)
+        else:
+            masked_proportions.append(read_mask_dict[read])
+        
 
     dust_summary = OrderedDict()
     dust_summary['nsamples'] = len(masked_proportions)
@@ -86,38 +95,32 @@ def dust_summarize(fastqin,windowsize=20,samplingfreq=0.01,max_sample_count=1000
 
 def write_dust_report(fqin,windowsize=20,samplingfreq=0.01,max_sample_count=100000):
     dust_summary=dust_summarize(fqin,windowsize=windowsize,samplingfreq=samplingfreq,max_sample_count=max_sample_count)
-    filestring = basename(fqin.replace('.fastq','').replace('.gz','')
-    report = open('%s_entropy.txt' % filestring,'w')
+    filestring = basename(fqin.replace('.fastq','').replace('.gz',''))
+    report = open('%s_dustmasker.txt' % filestring,'w')
     for key in dust_summary.keys()[:-1]:
-        report.write('%s:\t%s\n' % (key,dust_summary_keys[key])
+        report.write('%s:\t%s\n' % (key,dust_summary[key]))
     report.close()
     
     vals_R_vector='vals=c(%s)' % ','.join([str(val) for val in dust_summary['vector']])
     Rcode = open('%s_dustmaskerhist.R' % filestring,'w')
     Rcode.write('%s\n' % vals_R_vector)
     outpdf = '%s.dustmasker.pdf' % filestring
-    pdfcmd = 'pdf(file="%s.pdf")' % outpdf
-    plotcmd = 'hist(vals,breaks=20,freq=FALSE,xlab=\"Low complexity proportion by read\")'
+    pdfcmd = 'pdf(file="%s")' % outpdf
+    plotcmd = 'hist(vals,breaks=20,freq=FALSE,main="",xlab=\"Low complexity proportion by read\")'
     closecmd = 'dev.off()'
-    for cmd in [outpdf,pdfcmd,plotcmd,closecmd]:
-        Rcode.write('%s\n' % plotcmd)
-    
-    makeplot = 'Rscript %s_dustmaskerhist.R' % filestring
-    plot_run = Popen(makeplot,shell=True,stderr=PIPE,stdout=PIPE)
-        plotrun_out,plotrun_err=plot_run.communicate() 
-        if plot_run.returncode!=0:
-            raise Exception('Error generating dustmasker histogram for %s: %s\n' % (fqin,plotrun_err))
-
+    for cmd in [pdfcmd,plotcmd,closecmd]:
+        Rcode.write('%s\n' % cmd)
     Rcode.close()
 
-
+    makeplot = 'Rscript %s_dustmaskerhist.R' % filestring
+    call("%s" % makeplot,shell=True)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="options for generating dustmasker metrics from fastq_file")
     parser.add_argument('-i','--fastqin',dest='fqin',type=str,help='fastq infile')
     parser.add_argument('-f','--subsample_freq',default=0.01,dest='subsamp',type=float,help='proportion of sequences to sample')
-    parser.add_argument('-m','--max_samples',default=100000,dest=maxsamp,type=int,help='max number of reads to run dustmasker on')
-    parser.add_argument('-w','--dust_window_size',default=20,dest=winsize,type=int,help='dustmasker windowsize')
+    parser.add_argument('-m','--max_samples',default=100000,dest='maxsamp',type=int,help='max number of reads to run dustmasker on')
+    parser.add_argument('-w','--dust_window_size',default=20,dest='winsize',type=int,help='dustmasker windowsize')
     opts = parser.parse_args()
 
     write_dust_report(opts.fqin,windowsize=opts.winsize,samplingfreq=opts.subsamp,max_sample_count=opts.maxsamp)
