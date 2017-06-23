4de99e5497e33e82dbeb640e21b4d5537c1892f2
afreedman <adamfreedman@fas.harvard.edu>
2017-06-23 10:26:26 -0400

    sundry popen tweaks

--- odybcl2fastq/qc/fastqc_runner.py
+++ odybcl2fastq/qc/fastqc_runner.py
@@ -2,28 +2,31 @@ import argparse
 from subprocess import Popen,PIPE
 from glob import glob
 from os.path import basename
+from subprocess import call
 
 def fastqc_runner(fastq_dir,output_dir,numthreads = 1,batch = False):
     errors = []
     badfiles = []
-    
-    if batch = True:
-        files = ' '.join(glob('%s/[!Undetermined]*fastq.gz' % fastq_dir))
+    files = ' '.join(glob('%s/[!Undetermined]*fastq*' % fastq_dir)) 
+    print 'files are', files
+    if batch == True:
         fastqc_cmd = 'fastqc -o %s --threads %s -d %s/tmp %s' % (output_dir,numthreads,output_dir,files)
-        fastqc_run = Popen(cmd,shell=True,stderr=PIPE,stdout=PIPE)
+        fastqc_run = Popen(fastqc_cmd,shell=True,stderr=PIPE,stdout=PIPE)
         fastqc_out,fastqc_err=fastqc_run.communicate() 
         if fastqc_run.returncode!=0:
             errors.append(fastqc_err)
-            for file in files.split();
+            for file in files.split():
                 badfiles.append(basename(file))
     else:
-        for file in glob('%s/[!Undetermined]*fastq.gz' % fastq_dir):
-            fastqc_cmd = 'fastqc -o %s --threads 1 -d %s/tmp %s' % (output_dir,output_dir,file)
-            fastqc_run = Popen(cmd,shell=True,stderr=PIPE,stdout=PIPE)
+        for file in files.split():
+            print 'file is', basename(file)
+            call('mkdir %s/%s_tmp' % (output_dir,basename(file)),shell=True)
+            fastqc_cmd = 'fastqc -o %s --threads 1 -d %s/%s_tmp %s' % (output_dir,output_dir,basename(file),file)
+            fastqc_run = Popen(fastqc_cmd,shell=True,stderr=PIPE,stdout=PIPE)
             fastqc_out,fastqc_err=fastqc_run.communicate() 
             if fastqc_run.returncode!=0:
                 badfiles.append(basename(file))
-                errors.append(fastq_err)
+                errors.append(fastqc_err)
                 
     return badfiles,errors            
 
