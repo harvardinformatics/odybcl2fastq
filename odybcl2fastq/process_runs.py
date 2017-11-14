1989c9e08ea90c31b73a729110b48337aaf71bf9
portermahoney <portermahoney@gmail.com>
2017-11-14 15:52:09 -0500

    notify of incomplete runs

--- odybcl2fastq/process_runs.py
+++ odybcl2fastq/process_runs.py
@@ -25,7 +25,10 @@ OUTPUT_DIR = '/n/regal/informatics/mportermahoney/odytest/'
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOG_FILE = ROOT_DIR + '/odybcl2fastq.log'
 PROCESSED_FILE = 'odybcl2fastq.processed'
+COMPLETE_FILE = 'odybcl2fastq.complete'
+INCOMPLETE_NOTIFIED_FILE = 'odybcl2fastq.incomplete_notified'
 DAYS_TO_SEARCH = 7
+INCOMPLETE_AFTER_DAYS = 0
 # a hardcoded date not to search before
 # this will be helpful in transitioning from seqprep to odybcl2fastq
 SEARCH_AFTER_DATE = datetime.strptime('Nov 1 2017', '%b %d %Y')
@@ -50,14 +53,17 @@ def failure_email(run, cmd, ret_code, std_out, std_err):
     subject =  "Run Failed: %s" % run
     message = ("%s\ncmd: %s\nreturn code: %i\nstandard out: %s\nstandard"
             " error: %s\nsee log: %s\n" % (subject, cmd, ret_code, std_out, std_err, log))
+    send_email(message, subject)
+
+def send_email(message, subject):
     logging.warning(message)
     fromaddr = 'afreedman@fas.harvard.edu'
     toemaillist=['mportermahoney@g.harvard.edu']
     buildmessage(message, subject, None, fromaddr, toemaillist)
 
-def mark_processed(run_dir):
+def touch(run_dir, file):
     # touch a processed file in the run_dir
-    path = run_dir + PROCESSED_FILE
+    path = run_dir + file
     with open(path, 'w+'):
         os.utime(path, None)
 
@@ -79,14 +85,40 @@ def need_to_process(dir):
             return False
     return True
 
-def runs_to_process():
+def run_is_incomplete(dir):
+    now = datetime.now()
+    m_time = datetime.fromtimestamp(os.stat(dir).st_mtime)
+    # filter out if modified before cutover to odybcl2fastq
+    if m_time < SEARCH_AFTER_DATE:
+        return False
+    # filter out if modified after reasonable delay to allow for completion
+    if ((now - m_time).days) < INCOMPLETE_AFTER_DAYS:
+        return False
+    # filter out if tagged as complete
+    if os.path.isfile(dir + COMPLETE_FILE):
+        return False
+    # filter out if never tagged for processing
+    if not os.path.isfile(dir + PROCESSED_FILE):
+        return False
+    # filter out already notified
+    if os.path.isfile(dir + INCOMPLETE_NOTIFIED_FILE):
+        return False
+    return True
+
+def find_runs(filter):
     # get all subdirectories
     dirs = glob.glob(SOURCE_DIR + '*/')
     runs = []
     for dir in dirs:
-        if need_to_process(dir):
+        if filter(dir):
             runs.append(dir)
     return runs
+
+def runs_to_process():
+    return find_runs(need_to_process)
+
+def incomplete_runs():
+    return find_runs(run_is_incomplete)
 
 def get_odybcl2fastq_cmd(run_dir):
     run = os.path.basename(os.path.normpath(run_dir))
@@ -110,6 +142,15 @@ def run_odybcl2fastq(cmd):
     std_out, std_err = proc.communicate()
     return (proc.returncode, std_out, std_err, cmd)
 
+def notify_incomplete_runs():
+    run_dirs = incomplete_runs()
+    run_dirs_str = "\n".join(run_dirs)
+    message = "The following runs failed to complete %s or more days ago:\n\n%s" % (INCOMPLETE_AFTER_DAYS, run_dirs_str)
+    send_email(message, 'Odybcl2fastq incomplete runs')
+    for run in run_dirs:
+        touch(run, INCOMPLETE_NOTIFIED_FILE)
+
+
 def process_runs():
     runs_found = runs_to_process()
     proc_num = os.environ.get('ODYBCL2FASTQ_PROC_NUM', PROC_NUM)
@@ -123,7 +164,7 @@ def process_runs():
         cmd = get_odybcl2fastq_cmd(run_dir)
         logging.info("Queueing odybcl2fastq cmd for %s:\n%s\n" % (run, cmd))
         results[run] = pool.apply_async(run_odybcl2fastq, (cmd,))
-        mark_processed(run_dir) # mark so it doesn't get reprocessed
+        touch(run_dir, PROCESSED_FILE) # mark so it doesn't get reprocessed
     failed_runs = []
     success_runs = []
     for run, result in results.items():
@@ -134,6 +175,7 @@ def process_runs():
         else:
             failed_runs.append(run)
             failure_email(run, cmd, ret_code, std_out, std_err)
+        touch(run_dir, COMPLETE_FILE) # mark so it doesn't get reprocessed
     logging.info("Completed %i runs %i success %s and %i failures %s\n\n\n" %
             (len(results), len(success_runs), json.dumps(success_runs), len(failed_runs), json.dumps(failed_runs)))
 
@@ -143,6 +185,7 @@ if __name__ == "__main__":
         # run continuously
         while True:
             process_runs()
+            notify_incomplete_runs()
             # wait before checking for more runs to process
             frequency = os.getenv('ODYBCL2FASTQ_FREQUENCY', FREQUENCY)
             if frequency != FREQUENCY:
