c1c080f5378e16d298768660dc81b1f32b1fbf0a
portermahoney <portermahoney@gmail.com>
2017-11-16 11:34:38 -0500

    replace iter with findall elementtree version 1.2.6 does not have iter

--- odybcl2fastq/parsers/parse_runinfoxml.py
+++ odybcl2fastq/parsers/parse_runinfoxml.py
@@ -2,14 +2,12 @@ import xml.etree.ElementTree as ET
 from collections import OrderedDict
 
 def get_readinfo_from_runinfo(runinfo_xml_file):
+    print('read')
     tree = ET.parse(runinfo_xml_file)
     root = tree.getroot()
-
     read_dicts=[]
-
-    for child in root.iter('Reads'):
-        for child in child:
-            read_dicts.append(child.attrib)
+    for child in root.findall('.//Read'):
+        read_dicts.append(child.attrib)
     readkey_to_readdata_map=OrderedDict()
     for read_dict in read_dicts:
         number=read_dict.pop('Number')
