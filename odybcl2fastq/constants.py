72b961bff2fabdaeccd9826ac7611c65317a0cff
portermahoney <portermahoney@gmail.com>
2017-12-12 10:07:55 -0500

    move old test files, remove dup const file

--- odybcl2fastq/constants.py
+++ odybcl2fastq/constants.py
@@ -4,3 +4,4 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 TEMPLATE_DIR = BASE_DIR + '/templates'
 FASTQ_URL = 'https://software.rc.fas.harvard.edu/ngsdata/'
 FASTQ_DIR = '/n/ngsdata/'
+DATA_DIR = '/n/ngsdata/'
