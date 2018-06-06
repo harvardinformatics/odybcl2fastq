3fee51b20a0cb177ae39ac95053f571c6e841519
portermahoney <portermahoney@gmail.com>
2018-06-06 14:32:03 -0400

    email on exception from load_runs

--- odybcl2fastq/load_runs.py
+++ odybcl2fastq/load_runs.py
@@ -164,3 +164,4 @@ if __name__ == "__main__":
             time.sleep(frequency)
     except Exception as e:
         logging.exception(e)
+        send_email(str(e), 'Odybcl2fastq load_runs.py exception')
