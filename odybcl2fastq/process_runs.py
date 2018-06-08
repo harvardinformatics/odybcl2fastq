2edc1d313db25415bf7e0d10bb34a210766e22a6
portermahoney <portermahoney@gmail.com>
2018-06-08 09:13:02 -0400

    make proc num a env var

--- odybcl2fastq/process_runs.py
+++ odybcl2fastq/process_runs.py
@@ -35,7 +35,8 @@ INCOMPLETE_AFTER_DAYS = 2
 # this will be helpful in transitioning from seqprep to odybcl2fastq
 SEARCH_AFTER_DATE = datetime.strptime('Jan 15 2017', '%b %d %Y')
 REQUIRED_FILES = ['InterOp/QMetricsOut.bin', 'InterOp/TileMetricsOut.bin', 'RunInfo.xml', 'RTAComplete.txt']
-PROC_NUM = 2
+PROC_NUM = os.getenv('ODYBCL2FASTQ_PROC_NUM', 2)
+
 FREQUENCY = 60
 
 def setup_logging():
