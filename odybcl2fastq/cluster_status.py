6d76812a43ae6648da2ce1bbbcf60e7e3494419a
portermahoney <portermahoney@gmail.com>
2019-05-07 11:47:41 -0400

    fix

--- odybcl2fastq/cluster_status.py
+++ odybcl2fastq/cluster_status.py
@@ -6,8 +6,10 @@ Take from snakemake documentation
 https://snakemake.readthedocs.io/en/stable/tutorial/additional_features.html#using-cluster-status
 
 '''
+import sys
+import subprocess
 
-jobid = sys.argv[1]
+jobid = sys.argv[4]
 
 output = str(subprocess.check_output("sacct -j %s --format State --noheader | head -1 | awk '{print $1}'" % jobid, shell=True).strip())
 
