a5c73a0325cf858ff05015d1c193a288f63a5dba
portermahoney <portermahoney@gmail.com>
2019-10-07 12:37:52 -0400

    change cluster status to look for failed states

--- odybcl2fastq/cluster_status.py
+++ odybcl2fastq/cluster_status.py
@@ -16,9 +16,10 @@ cmd = "sacct -j %s --format State --noheader | head -1 | awk '{print $1}'" % job
 output = str(subprocess.check_output(cmd, shell=True).strip())
 
 running_status=["PENDING", "CONFIGURING", "COMPLETING", "RUNNING", "SUSPENDED"]
+failed_status=["FAILED", "OUT_OF_MEMORY", "TIMEOUT", "CANCELLED", "BOOT_FAIL", "DEADLINE", "NODE_FAIL", "PREEMPTED"]
 if "COMPLETED" in output:
     print("success")
-elif any(r in output for r in running_status):
-    print("running")
-else:
+elif any(r in output for r in failed_status):
     print("failed")
+else:
+    print("running")
