72a4ca98c01056fef263766e650177f61eff9622
Nathan Weeks <nweeks@g.harvard.edu>
2020-06-25 11:58:00 -0400

    Finish de-pandizing SampleSheet

--- odybcl2fastq/parsers/samplesheet.py
+++ odybcl2fastq/parsers/samplesheet.py
@@ -130,17 +130,13 @@ class SampleSheet(object):
 
     def validate_index2(self):
         corrected = False
-        fixed = self.get_samples()
-        if 'index2' in fixed.columns:
-            if fixed[fixed.index2!=''].empty:
-                # we have a totally empty index2 column so delete
-                fixed = fixed.drop('index2', axis = 1)
-                if 'I5_index_ID' in fixed.columns:
-                    fixed = fixed.drop('I5_index_ID', axis = 1)
-                corrected = True
-                fixed_rows = fixed.to_dict('records')
-                for i, k in enumerate(self.sections['Data'].keys()):
-                    self.sections['Data'][k] = OrderedDict(fixed_rows[i])
+        if len(self.sections['Data']) == sum(('index2','') in sample.items() for sample in self.sections['Data'].values()):
+            # we have a totally empty index2 column so delete
+            corrected = True
+            for sample in self.sections['Data']:
+                del self.sections['Data'][sample]['index2']
+                if 'I5_index_ID' in self.sections['Data'][sample]:
+                    del self.sections['Data'][sample]['I5_index_ID']
         return corrected
 
     def validate_sample_names(self):
@@ -267,13 +263,6 @@ class SampleSheet(object):
             if row['Sample_Project']:
                 projects.append(row['Sample_Project'])
         return projects
-
-    def get_samples(self):
-        samples = []
-        for key, row in self.sections['Data'].items():
-            if row['Sample_ID']:
-                samples.append(row['Sample_ID'])
-        return samples
 
     def has_poly_A_index(self):
         for k, row in self.sections['Data'].items():
